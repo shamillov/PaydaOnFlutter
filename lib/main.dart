13122bc9b609eddfe4b5c092a084423faaa791d9
shamillov <shamillov@bk.ru>
2020-06-10 17:26:37 +0300

    added BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,64 @@ class MainApp extends StatelessWidget {
         primaryColor: Colors.blue,
         textTheme: TextTheme(title: TextStyle(color: Colors.white))
       ),
-      home: HomePage(),
+      home: BottomNavigation(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-  
+class BottomNavigation extends StatefulWidget {
+  BottomNavigation({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return BottomNavigationState();
+  }
+}
+
+class BottomNavigationState extends State<BottomNavigation> {
+  int selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(title: Text("Payda")),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Payda"),
+        centerTitle: true,
+      ),
+      body: Center(
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text("Profile")
+          )
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: onItemTapped,
       ),
     );
   }
+}
 
+class ActiveDonationsList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
 }
