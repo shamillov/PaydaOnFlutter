a8a777b3d95f3f492f58bc299381e6d73d653a3c
shamillov <shamillov@bk.ru>
2020-06-16 17:41:44 +0300

    added ListView for completed donation page

--- lib/screens/activeDonationPage.dart
+++ lib/screens/activeDonationPage.dart
@@ -43,9 +43,11 @@ class ActiveDonationList extends StatelessWidget {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text("Название фонда"),
-                                Text("Локация", style: TextStyle(fontSize: 12, color: Colors.black54),)
+                                Text("Локация", style: TextStyle(fontSize: 12, color: Colors.black54))
                               ],
                             ),
+                            Spacer(),
+                            Align(alignment: Alignment.centerRight, child: Icon(Icons.reply, color: Colors.grey))
                           ],
                         ),
                       ),
@@ -61,17 +63,17 @@ class ActiveDonationList extends StatelessWidget {
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.end,
                               children: [
-                                Text("131123", style: TextStyle(fontSize: 18, color: Colors.black54, fontWeight: FontWeight.bold),),
-                                Text("собрано", style: TextStyle(fontSize: 12, color: Colors.black54),)
+                                Text("131123", style: TextStyle(fontSize: 18, color: Colors.black54, fontWeight: FontWeight.bold)),
+                                Text("собрано", style: TextStyle(fontSize: 12, color: Colors.black54))
                               ],
                             ),
                             Container(
-                              margin: EdgeInsets.only(left: 8, right: 8),
+                              margin: EdgeInsets.only(left: 10, right: 10),
                               child: RaisedButton(
                                 onPressed: (){
                                   Scaffold.of(context).showSnackBar(
                                     SnackBar(
-                                      content: Text("Button is clicked"),
+                                      content: Text("Button is clicked: $index"),
                                     )
                                   );
                                 },
--- lib/screens/completedDonationPage.dart
+++ lib/screens/completedDonationPage.dart
@@ -1,15 +1,94 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CompletedDonationPage  extends StatelessWidget {
+class CompletedDonationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Completed Donation",
-          style: TextStyle(color: Colors.black54, fontSize: 22, fontStyle: FontStyle.italic),
-        ),
+      body: CompletedDonationList(),
+    );
+  }
+}
+
+class CompletedDonationList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return Card(
+              margin: EdgeInsets.all(6),
+              child: Container(
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(6),
+                      child: Row(
+                        children: [
+                          Container(
+                            child: Icon(Icons.person),
+                            margin: EdgeInsets.only(right: 4),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Название фонда"),
+                              Text("Локация", style: TextStyle(fontSize: 12, color: Colors.black54))
+                            ],
+                          ),
+                          Spacer(),
+                          Align(alignment: Alignment.centerRight, child: Icon(Icons.reply, color: Colors.grey))
+                        ],
+                      ),
+                    ),
+                    Container(
+                        height: 300,
+                        color: Colors.grey
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 6),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text("131123", style: TextStyle(fontSize: 18, color: Colors.black54, fontWeight: FontWeight.bold)),
+                              Text("собрано", style: TextStyle(fontSize: 12, color: Colors.black54))
+                            ],
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 8, right: 8),
+                            child: RaisedButton(
+                              color: Colors.blue,
+                              textColor: Colors.white,
+                              child: Text("Сбор завершен"),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("1000000", style: TextStyle(fontSize: 18, color: Colors.black54, fontWeight: FontWeight.bold)),
+                              Text("цель", style: TextStyle(fontSize: 12, color: Colors.black54))
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                    Center(
+                      child: Text("Название сбора", style: TextStyle(fontSize: 18, color: Colors.black54, fontWeight: FontWeight.bold)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(6),
+                      child: Text("Описание сбора, описание сбора"),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
       ),
     );
   }
